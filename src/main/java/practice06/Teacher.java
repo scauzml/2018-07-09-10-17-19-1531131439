980e5039bb0754668c857f32f348499b53820d12
scauzml <13828607279@163.com>
2019-07-11 19:22:09 +0800

    practice06提交

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,31 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    public int klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        if(this.klass==0){
+              return super.introduce()+" I am a Teacher. I teach No Class.";
+        }
+        return super.introduce()+" I am a Teacher. I teach Class "+this.klass+".";
+    }
 }
